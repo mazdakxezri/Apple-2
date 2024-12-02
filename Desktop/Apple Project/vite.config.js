1b8410def8e5b94a940779acc702244349680188
Mazdak <mazdakxezri74@gmail.com>
2024-12-02 16:53:08 +0330

    update

--- Desktop/Apple Project/vite.config.js
+++ Desktop/Apple Project/vite.config.js
@@ -1,18 +1,22 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-d7u",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "jsm-d7u",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-d7u",
+      project: "javascript-react",
+    }),
+    sentryVitePlugin({
+      org: "jsm-d7u",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
